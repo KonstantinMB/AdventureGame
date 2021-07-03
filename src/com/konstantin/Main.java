8504ab756cbde3cf0200b8baf96481eb829e1a0d
Konstantin Borimechkov <konstantinmb@yahoo.com>
2021-07-03 12:09:07 +0300

    Commiting changes (immutable classes)

--- src/com/konstantin/Main.java
+++ src/com/konstantin/Main.java
@@ -13,28 +13,33 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         //now, lets add some locations to the map:
-        locations.put(0, new Location(0, "At home"));
-        locations.put(1, new Location(1, "Lifting weights at the gym"));
-        locations.put(2, new Location(2, "Watching educational videos."));
-        locations.put(3, new Location(3, "Going for a walk"));
-        locations.put(4, new Location(4, "At your villa"));
-        locations.put(5, new Location(5, "Chilling at the club"));
+        Map<String,Integer> tempExit = new HashMap<String,Integer>();
+        locations.put(0, new Location(0, "At home",tempExit));
 
-        //The rules of the game:
-        locations.get(1).addExit("W",2);
-        locations.get(1).addExit("E",3);
-        locations.get(1).addExit("S",4);
-        locations.get(1).addExit("N",5);
+        tempExit = new HashMap<String,Integer>();
+        tempExit.put("W", 2);
+        tempExit.put("E",3);
+        tempExit.put("S",4);
+        tempExit.put("N",5);
+        locations.put(1, new Location(1, "Lifting weights at the gym",tempExit));
 
-        locations.get(2).addExit("N",5);
+        tempExit = new HashMap<String,Integer>();
+        tempExit.put("N",5);
+        locations.put(2, new Location(2, "Watching educational videos.",tempExit));
 
-        locations.get(3).addExit("W",1);
+        tempExit = new HashMap<String,Integer>();
+        tempExit.put("W",1);
+        locations.put(3, new Location(3, "Going for a walk",tempExit));
 
-        locations.get(4).addExit("N",1);
-        locations.get(4).addExit("W",2);
+        tempExit = new HashMap<String,Integer>();
+        tempExit.put("N",1);
+        tempExit.put("W",2);
+        locations.put(4, new Location(4, "At your villa",tempExit));
 
-        locations.get(5).addExit("S",1);
-        locations.get(5).addExit("W",2);
+        tempExit = new HashMap<String,Integer>();
+        tempExit.put("S",1);
+        tempExit.put("W",2);
+        locations.put(5, new Location(5, "Chilling at the club",tempExit));
 
         // adding more types of inputs that the user can type in order to give his next location
         Map<String,String> vocabulary = new HashMap<String, String>();
